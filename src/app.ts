3107f4c62744bb5e409aa62760a1cb3755280450
rinx01 <rinxdev2023@gmail.com>
2023-11-20 16:23:43 +0700

    🆙Health check route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import type { Request, Response } from "express";
 import routers from "./routers";
 import 'dotenv/config'
 
@@ -6,6 +7,10 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use("/", routers());
+
+app.get("/healthz", (req: Request, res: Response) => {
+  res.status(200).end()
+})
 
 app.listen(PORT, () => {
   console.log("Server running ", PORT);
